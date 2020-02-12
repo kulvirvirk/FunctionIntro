2bd54ea0a9f78d3d1c31244aa39c937784b8bc06
kulvirvirk <kvirkdev@gmail.com>
2020-02-12 01:35:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+#create a function that gets username and then greets them
+
+
+def greet():
+  first_name = input("Enter your first name: ")
+  last_name = input("Enter your last name: ")
+  print("Hello " + first_name + " " + last_name + "!")
+
+#call the function 
+greet()
