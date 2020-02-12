e5d61e6caa3f13a22ed4589ef4e06bae34dfa7fe
kulvirvirk <kvirkdev@gmail.com>
2020-02-12 01:35:50 +0000

    added comments

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 #create a function that gets username and then greets them
 
 
+#define the function
+#store user's first name in variable first_name
+#store user's last name in variable last_name
+#Print the full name with greetings
 def greet():
   first_name = input("Enter your first name: ")
   last_name = input("Enter your last name: ")
